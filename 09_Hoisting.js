5c185225ccd359d4bc145b5e8be79c7b073f38a0
Piotr <piotr_as@yahoo.com>
2018-07-05 17:48:45 +0200

    Added lesson 09 nad update html

---
+++ 09_Hoisting.js
@@ -0,0 +1,10 @@
+//    ------------------------ Hoisting ------------------------    //
+// Podnoszenie  /  windowanie
+// Hoisting: przenoszenie deklaracji na początek kodu
+
+console.log(a);
+
+var a = 5;
+
+// Najpierw hoistingowi ulegają funkcje
+
