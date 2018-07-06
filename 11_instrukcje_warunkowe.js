f13aa5720a773f0d8d56ffbc6c57357b8cdceb6c
Piotr <piotr_as@yahoo.com>
2018-07-06 15:58:39 +0200

    Added begginnin lesson 11

--- 11_instrukcje_warunkowe.js
+++ 11_instrukcje_warunkowe.js
@@ -6,3 +6,36 @@
 
 // Jeśli warunek nie będzie typem Boolean to będzie do niego skonwetowany
 
+// inatrukcja else  if () kończy się w momencie kiedy jakiś warunek będzie true
+
+// Można zawrzeć wiele warunków
+
+if (false) 
+{
+    console.log("wykonałem if");
+} 
+else if (true)
+{
+    console.log("wykonałem 'else if'");
+}
+
+// ify można zagnieżdżać
+
+// if (wrunek) {
+//     if (warunek) {
+//         kod
+//     }
+//     else{
+//         kod
+//         if (warunek)
+//         {
+//             kod
+//         }
+//     }
+// }
+
+//  ------------------------ OPERATOR WARUNKOWY ------------------------  //
+
+(warunek) ? kod1 : kod2
+
+warunek -> true -> wykonaj i zwróć kod1warunek -> false -> wykonaj i zwróć kod2
