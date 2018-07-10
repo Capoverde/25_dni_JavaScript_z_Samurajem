839102f1fba5014643c85511f38ec7c652ab5a5f
PiotrAs <piotr.slobinski@gmail.com>
2018-07-10 18:10:36 +0200

    updated index.html, style.css, lesson 15

---
+++ 15_Callbacks.js
@@ -0,0 +1,18 @@
+//  ---------------- Funkcja jako First Class Object ------------- //
+
+// Znaczy to, ze funkcje są pełnoprawnymi obiektami i mogą być wykorzystywane jak inne obiekty.
+// Funkcje mogą być np. przekazywane do innej funkcji jako argument, czy zwracane z nich  dokładnie tak jak inne wartości.
+
+// ---------------- Callback (funkcja wywołania zwrotnego) -------------- //
+
+// Jest to funkcja, którą przekazujemy jako argument do funkcji, którą wywowłujemy
+
+// function (a,b, jakasFunkcja){
+//   jakasFunkcja to callback
+// }
+
+// ----------------- Funkcja wyszego rzędu (wyzszego poziomu) ----------- //
+
+// Funkcja do której przekazujemy callback jest funkcją wyszego rzędu
+// (jezeli funkcja zawiera callback, to jest funkją wyzszego rzedu)
+
